826577ba8d437b206cac24d90dde335e1ee75b0f
Mike Hall <mike@just3ws.com>
2014-06-18 16:59:51 -0500

    Only run sitemap_generator in production

--- config/initializers/sitemap_generator.rb
+++ config/initializers/sitemap_generator.rb
@@ -1,11 +1,13 @@
-SitemapGenerator::Sitemap.default_host = 'https://ugl.st'
-SitemapGenerator::Sitemap.sitemaps_host = "http://uglst-#{Rails.env}.s3.amazonaws.com/"
-SitemapGenerator::Sitemap.public_path = 'tmp/'
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
+if Rails.env.production?
+  SitemapGenerator::Sitemap.default_host = 'https://ugl.st'
+  SitemapGenerator::Sitemap.sitemaps_host = "http://uglst-#{Rails.env}.s3.amazonaws.com/"
+  SitemapGenerator::Sitemap.public_path = 'tmp/'
+  SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+  SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 
-SitemapGenerator::Sitemap.create do
-  #UserGroup.find_each do |ug|
+  SitemapGenerator::Sitemap.create do
+    #UserGroup.find_each do |ug|
     #add user_groups_path(ug)
-  #end
+    #end
+  end
 end

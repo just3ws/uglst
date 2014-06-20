3c3f13ef6b23742ca9f85e08ccb12fd10a658e69
Mike Hall <mike@just3ws.com>
2014-06-20 17:01:08 -0500

    Handle registrations.js asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( user_groups.js )
 Rails.application.config.assets.precompile += %w( sessions.js )
 Rails.application.config.assets.precompile += %w( profiles.js )
+Rails.application.config.assets.precompile += %w( registrations.js )

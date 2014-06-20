71bac8fd935b230a082f2a75a442ad49e9df06fc
Mike Hall <mike@just3ws.com>
2014-06-19 20:37:47 -0500

    Added poltergeist config to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@ require 'bitfields/rspec'
 
 require 'sucker_punch/testing/inline'
 
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }

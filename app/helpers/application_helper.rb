bee24d2f83f2717e61ae1463425d253cf8b49456
Mike Hall <mike@just3ws.com>
2014-06-23 11:36:09 -0500

    Tidied up the login/registration forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
     Rails.logger.info "[REMINDER TO]: #{note}. (#{caller.first})"
     yield if block_given? unless Rails.env.production?
   end
+
+  def display_base_errors resource
+    return '' if (resource.errors.empty?) or (resource.errors[:base].empty?)
+    messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
+    html = <<-HTML
+    <div class="alert alert-error alert-block">
+      <button type="button" class="close" data-dismiss="alert">&#215;</button>
+    #{messages}
+    </div>
+    HTML
+    html.html_safe
+  end
 end

02bd7f24ac21e28505d9e230624d587e1b521e57
Mike Hall <mike@just3ws.com>
2014-06-30 16:32:46 -0500

    Configure Carrierwave per environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,23 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  }
   config.fog_directory   = "uglst-#{Rails.env}"
   config.cache_dir       = "#{Rails.root}/tmp/uploads"
   config.fog_attributes  = { 'Cache-Control' => 'max-age=315576000' }
-  config.storage         = :fog
+
+  if Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+  end
+
+  if Rails.env.development?
+    config.storage = :file
+  end
+
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  end
 end

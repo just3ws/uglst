a32a9dfd2a264e31c8759cbbf407124658d8fc90
Mike Hall <mike@just3ws.com>
2014-07-30 02:18:50 -0500

    Simple import of the raw data for php.ug

---
+++ db/migrate/20140730061759_create_import_php_ugs.rb
@@ -0,0 +1,10 @@
+class CreateImportPhpUgs < ActiveRecord::Migration
+  def change
+    create_table :import_php_ugs do |t|
+      t.integer :php_ug_id
+      t.json :php_ug_data
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730050821) do
+ActiveRecord::Schema.define(version: 20140730061759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20140730050821) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
+  create_table "import_php_ugs", force: true do |t|
+    t.integer  "php_ug_id"
+    t.json     "php_ug_data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "network_affiliations", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
     t.uuid     "network_id"

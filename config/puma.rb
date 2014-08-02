9366c01173c694e8ccbc26481bc531931eab755e
Mike Hall <mike@just3ws.com>
2014-08-02 11:18:58 -0500

    Removed puma config

--- config/puma.rb
+++
@@ -1,6 +0,0 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS'] || 1), Integer(ENV['MAX_THREADS'] || 16)
-
-rackup DefaultRackup
-port ENV['PORT'] || '4321'
-environment ENV['RACK_ENV'] || 'development'

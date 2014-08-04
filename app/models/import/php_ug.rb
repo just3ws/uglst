32243611a9ef437d7cac9c901259a126a9a0b437
Mike Hall <mike@just3ws.com>
2014-08-04 16:11:24 -0500

    Speccing out the PHP.ug import functionality and wrapping the calls with VCR cassettes

--- app/models/import/php_ug.rb
+++ app/models/import/php_ug.rb
@@ -2,15 +2,21 @@ class Import::PhpUg < ActiveRecord::Base
 
   URL ||= 'http://php.ug/api/rest/listtype.json/1'
 
-  def self.fetch_and_store
+  def self.fetch
     response = RestClient.get(URL, accept: :json)
-    fail "Could not fetch from #{URL} because received HTTP status #{response.code}" unless response.code == 200
-    data = MultiJson.load(response)['groups']
 
-    data.each do |user_group|
-    Import::PhpUg.find_or_create_by(php_ug_id: Integer(user_group['id'])) do |m|
-      m.php_ug_data = MultiJson.dump(user_group)
+    unless 200 == response.code
+      fail "Could not fetch from #{URL} because received HTTP status #{response.code}"
     end
+
+    MultiJson.load(response)['groups']
+  end
+
+  def self.store(user_groups)
+    user_groups.each do |user_group|
+      Import::PhpUg.find_or_create_by(php_ug_id: Integer(user_group['id'])) do |m|
+        m.php_ug_data = MultiJson.dump(user_group)
+      end
     end
   end
 end
--- spec/models/import/php_ug_spec.rb
+++ spec/models/import/php_ug_spec.rb
@@ -1,6 +1,15 @@
 RSpec.describe Import::PhpUg, type: :model do
   it 'fetches the user-groups from php.ug' do
-    VCR.use_cassette('php_ug') do
+    VCR.use_cassette('php_ug', record: :new_episodes) do
+
+      expect(Import::PhpUg.count).to eq(0)
+
+      fetched = Import::PhpUg.fetch
+      Import::PhpUg.store(fetched)
+      #Import::PhpUg.untransformed # fetch all the php.ug re
+
+      expect(Import::PhpUg.count).to be > 0
+
     end
   end
 end

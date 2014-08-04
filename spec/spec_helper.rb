22aba02b0df26600e317ddb9bf0c28280fcae521
Mike Hall <mike@just3ws.com>
2014-08-04 15:15:32 -0500

    Updated spec_helper with a configuration change to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,13 +39,4 @@ RSpec.configure do |config|
   config.order                                      = 'random'
   config.tty                                        = true
   config.use_transactional_fixtures                 = true
-  config.mock_with :rspec do |mocks|
-
-    # This option should be set when all dependencies are being loaded
-    # before a spec run, as is the case in a typical spec helper. It will
-    # cause any verifying double instantiation for a class that does not
-    # exist to raise, protecting against incorrectly spelt names.
-    mocks.verify_doubled_constant_names = true
-
-  end
 end

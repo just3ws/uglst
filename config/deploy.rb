609dbf82ee732e2899c6829accf2c360f8b04a27
Mike Hall <mike@just3ws.com>
2014-08-05 23:43:45 -0500

    Restore default for linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 

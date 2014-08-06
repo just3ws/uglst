66e94c0f7a3a0e92224a30db4aa69734b37cc3f3
Mike Hall <mike@just3ws.com>
2014-08-05 23:28:36 -0500

    Added cap rvm and bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,15 +10,6 @@ set :repo_url, 'git@github.com:ugtastic/uglst.git'
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/ugtastic/uglst'
 
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :pretty
-set :format, :pretty
-
-# Default value for :log_level is :debug
-set :log_level, :debug
-
 # Default value for :pty is false
 set :pty, true
 
@@ -29,10 +20,9 @@ set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
-set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-set :keep_releases, 5
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.1.2@uglst'
 
 namespace :deploy do
 

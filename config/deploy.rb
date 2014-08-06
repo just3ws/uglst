a388fa47fe95e9534cb9678ac7d81c1ae0fc70eb
Mike Hall <mike@just3ws.com>
2014-08-06 11:14:37 -0500

    Added puma config to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,10 +19,24 @@ set :linked_files, %w{config/database.yml config/newrelic.yml .env}
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for default_env is {}
-
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.1.2@uglst'
+
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+set :puma_preload_app, true
+set :puma_init_active_record, true
 
 namespace :deploy do
 

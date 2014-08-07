ac7ad254577eb074748b07a9e65d56034fa947af
Mike Hall <mike@just3ws.com>
2014-08-07 09:30:41 -0500

    Fixed broken ip address reference

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db,  %w(ugtastic@162.243.77.132)
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'ugl.st', user: 'ugtastic', roles: %w(web app db)
+server '162.243.77.132', user: 'ugtastic', roles: %w(web app db)
 
 # Custom SSH Options
 # ==================

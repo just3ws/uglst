6d3e84a2cf2ffe7bbed8e0216c5c0ec462be43c2
Mike Hall <mike@just3ws.com>
2014-08-10 23:17:09 -0500

    Adding feature specs to get the UI under test

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,6 @@
+describe 'The Login Process', type: :feature do
+  before {}
+
+  it 'logs me in' do
+  end
+end
---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,25 @@
+describe 'The Signup Process', type: :feature do
+  it "let's me create a new account" do
+
+    data = {username: Faker::Internet.user_name.classify.underscore}
+
+    sign_up_steps(data)
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    within ('#nav-profile-btn') { expect(page).to have_content(data[:username]) }
+  end
+end
+
+def sign_up_steps(data)
+  visit root_path
+
+  click_link('Sign up')
+
+  fill_in('Username', with: data[:username])
+  fill_in('Email', with: Faker::Internet.email)
+  password = 'password'
+  fill_in('user_password', with: password)
+  fill_in('user_password_confirmation', with: password)
+
+  click_button('Sign up')
+end

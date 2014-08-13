0ed76e2e66353c203006f74c3f1d21b3b3c50a67
Mike Hall <mike@just3ws.com>
2014-08-13 17:53:35 -0500

    Merged the clockwork cap config to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,6 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-require 'deploy/clockwork'
-
 set :application, 'uglst'
 set :repo_url, 'git@github.com:ugtastic/uglst.git'
 set :deploy_to, '/var/ugtastic/uglst'
@@ -30,5 +28,39 @@ namespace :deploy do
       #   execute :rake, 'cache:clear'
       # end
     end
+  end
+end
+
+after 'deploy:stop', 'clockwork:stop'
+after 'deploy:start', 'clockwork:start'
+after 'deploy:restart', 'clockwork:restart'
+
+namespace :clockwork do
+  desc 'Stop clockwork'
+  task :stop, roles: clockwork_roles, on_error: :continue, on_no_matching_servers: :continue do
+    run "if [ -d #{current_path} ] && [ -f #{pid_file} ]; then cd #{current_path} && kill -INT `cat #{pid_file}` ; fi"
+  end
+
+  desc 'Start clockwork'
+  task :start, roles: clockwork_roles, on_no_matching_servers: :continue do
+    run "daemon --inherit --name=clockwork --env='#{rails_env}' --output=#{log_file} --pidfile=#{pid_file} -D #{current_path} -- bundle exec clockwork config/clockwork.rb"
+  end
+
+  desc 'Restart clockwork'
+  task :restart, roles: clockwork_roles, on_no_matching_servers: :continue do
+    stop
+    start
+  end
+
+  def rails_env
+    fetch(:rails_env, false) ? "RAILS_ENV=#{fetch(:rails_env)}" : ''
+  end
+
+  def log_file
+    fetch(:clockwork_log_file, "#{current_path}/log/clockwork.log")
+  end
+
+  def pid_file
+    fetch(:clockwork_pid_file, "#{current_path}/tmp/pids/clockwork.pid")
   end
 end

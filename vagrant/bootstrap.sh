82d0ad9c2fb7bb85b5949218ef37b1825e00039e
Mike Hall <mike@just3ws.com>
2014-08-29 10:09:46 -0500

    Updated the script/ide and gems

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -8,12 +8,18 @@ echo I think my home is $HOME
 source /home/vagrant/app/vagrant/install.sh
 
 # Postgres
+# -- create the application user
 su -c "createuser -s uglst" postgres
 
 # -- Enable accessing Postgres from the host machine
 echo "listen_addresses = '*'" | tee -a /etc/postgresql/9.3/main/postgresql.conf
 echo host all all  0.0.0.0/0 trust | tee -a /etc/postgresql/9.3/main/pg_hba.conf
-
 service postgresql restart
+
+# Redis
+# -- Enable accessing Redis from the host machine
+sed -i.bak "s/^bind 127.0.0.1/# bind 127.0.0.1/g" /etc/redis/redis.conf
+service redis-server restart
+
 
 su -c "source /home/vagrant/app/vagrant/user-config.sh" vagrant

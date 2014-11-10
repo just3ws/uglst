3bacc58fb3f0c5d76755c98c7d1ec16c87a9a238
Mike Hall <mike@just3ws.com>
2014-11-09 21:07:34 -0600

    Use the screen_name when tweeting a user-group

--- app/jobs/user_group_tweeter_job.rb
+++ app/jobs/user_group_tweeter_job.rb
@@ -12,24 +12,29 @@ class UserGroupTweeterJob
 
   def send_tweet!(ug)
     client = ::Twitter::REST::Client.new do |config|
-      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-      config.consumer_key = ENV['TWITTER_API_KEY']
-      config.consumer_secret = ENV['TWITTER_API_SECRET']
+      config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_API_SECRET']
     end
 
     options = {}
     if ug.latitude && ug.longitude
-      options[:lat] = ug.latitude.to_f
-      options[:long] = ug.longitude.to_f
+      options[:lat]                 = ug.latitude.to_f
+      options[:long]                = ug.longitude.to_f
       options[:display_coordinates] = true
     end
 
     # media = File.new(ug.logo.present? ? open("https://ugl.st#{ug.logo.url}") : "#{Rails.root}/app#{ActionController::Base.helpers.asset_path('/assets/images/user_group_avatar_fallback.png', digest: false)}")
 
-    tweet = "#{ug.name} #{ug.twitter.present? ? "by @#{ug.twitter} " : '' } #UserGroup #{Rails.application.routes.url_helpers.user_group_url(ug, host: 'ugl.st')} via @uglst"
+    tweet = self.class.tweet(ug)
 
     # client.update_with_media(tweet, media , options)
     client.update(tweet, options) if Rails.env.production?
+  end
+
+  def self.tweet(ug)
+    msg = "#{ug.name} #{if ug.twitter.present? then "by @#{ug.twitter.screen_name} " else '' end} #UserGroup #{Rails.application.routes.url_helpers.user_group_url(ug, host: 'ugl.st')} via @uglst"
+    msg.gsub(/\s+/, ' ').strip
   end
 end

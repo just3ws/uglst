43f1fd35f2b43fa774520ff328b02c278d090a39
Mike Hall <mike@just3ws.com>
2014-11-22 02:13:24 -0600

    Tweak the number of items to display on pagination

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,6 @@
 Kaminari.configure do |_config|
-  # config.default_per_page = 25
-  # config.max_per_page = nil
+  config.default_per_page = 9
+  config.max_per_page = 13
   # config.window = 4
   # config.outer_window = 0
   # config.left = 0

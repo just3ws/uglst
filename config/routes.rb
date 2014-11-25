b3851e08e8133fac3c7589a40f296582d00f1f6b
Mike Hall <mike@just3ws.com>
2014-11-25 00:32:49 -0600

    Tidied up the routes and inflections for future API additions

--- config/routes.rb
+++ config/routes.rb
@@ -56,31 +56,36 @@
 
 require 'sidekiq/web'
 
+# 127.0.0.1 uglst.dev
+# 127.0.0.1 api.uglst.dev
+
 Rails.application.routes.draw do
-
-  authenticate :user, ->(u) { u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
-
-  resources :networks
-
-  get '/status', to: 'status#ping'
+  get '/status',            to: 'status#ping'
   get '/heartbeat.:format', to: 'heartbeat#ping', constraints: { format: 'txt' }
 
-  mount(MailPreview => 'mail_view') if Rails.env.development?
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => '/admin/sidekiq'
+  end
+
+  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+  end
 
   get 'pages/privacy'
   get 'pages/terms_of_service'
 
+  resources :networks
+  resources :profiles
+  devise_for :users
+
   resources :user_groups do
-    post '/join' => 'user_groups#join'
-    post '/leave' => 'user_groups#leave'
+    post '/join'        => 'user_groups#join'
+    post '/leave'       => 'user_groups#leave'
     get '/memberships' => 'user_groups#memberships'
   end
 
-  resources :profiles
-
-  devise_for :users
-
   root 'pages#root'
+
+  if Rails.env.development?
+    mount(MailPreview => 'mail_view')
+  end
 end

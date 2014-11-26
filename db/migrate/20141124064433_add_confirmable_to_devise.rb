49fde295dd5bc03d36887d2086493f02915dc81f
Mike Hall <mike@just3ws.com>
2014-11-26 01:34:12 -0600

    Set the grandfathering of older accounts confirmation

--- db/migrate/20141124064433_add_confirmable_to_devise.rb
+++ db/migrate/20141124064433_add_confirmable_to_devise.rb
@@ -11,7 +11,8 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
-    User.update_all(confirmed_at: Time.now)
+    ts = DateTime.now
+    User.update_all(confirmed_at: ts, confirmation_sent_at: ts, confirmation_token: 'backfilled')
     # All existing user accounts should be able to log in after this.
   end
 

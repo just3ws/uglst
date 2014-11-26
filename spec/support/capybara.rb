dc6702871940da5bc7c164c0c1b4ec00936ab1b1
Mike Hall <mike@just3ws.com>
2014-11-25 21:48:42 -0600

    Restored PhantomJS driver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,6 @@
 require 'capybara'
 require 'capybara/rspec'
-# require 'capybara/poltergeist'
+require 'capybara/poltergeist'
 
 require 'turnip'
 require 'turnip/capybara'
@@ -9,22 +9,22 @@ require 'turnip/rspec'
 require 'capybara-screenshot'
 require 'capybara-screenshot/rspec'
 
-# Capybara.register_driver :poltergeist do |app|
-# Capybara::Poltergeist::Driver.new(app, {
-# js_errors: true,
-# default_wait_time: 30,
-# timeout: 100,
-# inspector: true,
-# debug: true,
-# phantomjs_options: %w(--load-images=no --ignore-ssl-errors=yes)
-# })
-# end
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+    debug:             false,
+    default_wait_time: 30,
+    inspector:         false,
+    js_errors:         false,
+    phantomjs_options: %w(--load-images=no --ignore-ssl-errors=yes),
+    timeout:           1000
+  })
+end
 
 Capybara.configure do |c|
-  # c.default_driver         = :poltergeist
-  # c.javascript_driver      = :poltergeist
-  c.ignore_hidden_elements = false
+  c.default_driver         = :poltergeist
   c.default_wait_time      = 30
+  c.ignore_hidden_elements = false
+  c.javascript_driver      = :poltergeist
 end
 
 Dir[Rails.root.join('spec/acceptance/steps/**/*.rb')].each { |f| require f }

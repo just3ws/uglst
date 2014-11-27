0d3f58b9356ce3d5d6c3de69417b1bed5a4fa52f
Mike Hall <mike@just3ws.com>
2014-11-26 23:06:52 -0600

    updated vagrant bootstrap

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,14 +7,21 @@ source /home/vagrant/app/vagrant/install.sh
 su - vagrant <<-'EOF'
   echo export EDITOR=vim >> $HOME/.bashrc
 
+  echo export EDITOR=vim >> $HOME/.bashrc
   echo insecure > $HOME/.curlrc
+  echo progress-bar >> $HOME/.curlrc
+  echo gem: --no-document >> $HOME/.gemrc
 
   echo rvm_install_on_use_flag=1 >> $HOME/.rvmrc
   echo rvm_project_rvmrc=1 >> $HOME/.rvmrc
   echo rvm_trust_rvmrcs_flag=1 >> $HOME/.rvmrc
-  curl -k -L https://get.rvm.io | bash -s stable --autolibs=enabled
+
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+
+  curl -k -L https://get.rvm.io | bash -s stable --autolibs=install-packages
   source "$HOME/.rvm/scripts/rvm"
   [[ -s "$rvm_path/hooks/after_cd_bundle" ]] && chmod +x $rvm_path/hooks/after_cd_bundle
+  rvm autolibs enable
   rvm requirements
   rvm reload
 
@@ -24,19 +31,16 @@ su - vagrant <<-'EOF'
   rvm use $_RUBY_VERSION --default
   rvm use $_RUBY_VERSION@uglst
 
-  cd $HOME/app
-  gem update --system
-  gem update bundler
-  bundle config --global jobs 3
-  bundle install
-
   cd ~/app
+
+  bundle config --global jobs 3
+  gem update --system
+  gem install bundler
   bundle check || bundle install
+
   # Force the app to use the internal Postgres port number and ignore .env
-  export DEVELOPMENT_POSTGRES_PORT=5432
-  DEVELOPMENT_POSTGRES_PORT=5432 bundle exec rake db:migrate
-  DEVELOPMENT_POSTGRES_PORT=5432 bundle exec rake db:test:prepare
-  DEVELOPMENT_POSTGRES_PORT=5432 bundle exec rake db:create:all
-  DEVELOPMENT_POSTGRES_PORT=5432 bundle exec rake db:migrate
-  DEVELOPMENT_POSTGRES_PORT=5432 bundle exec rake db:seed
+  RAILS_ENV=test bundle exec rake db:create
+  bundle exec rake db:create:all
+  bundle exec rake db:migrate
+  bundle exec rake db:seed
 EOF

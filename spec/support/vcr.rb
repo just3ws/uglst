f2c889977a3805db777309559a3bf451748b526f
Mike Hall <mike@just3ws.com>
2014-12-02 00:35:34 -0600

    Moved VCR cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,5 +1,5 @@
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'spec/fixtures/cassettes'
   c.hook_into :webmock
   c.ignore_localhost = true
   c.default_cassette_options = { record: :new_episodes }

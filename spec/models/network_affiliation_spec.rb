e08b41c4b8fa74e41cef5e8bb0d797dacfd0f8b2
Mike Hall <mike@just3ws.com>
2014-12-05 12:49:01 -0600

    Added spec placeholders

---
+++ spec/models/network_affiliation_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe NetworkAffiliation do
+  let(:network_affiliation) do
+    NetworkAffiliation.new
+  end
+end
--- spec/models/network_spec.rb
+++ spec/models/network_spec.rb
@@ -1,19 +1,7 @@
-RSpec.describe Network, type: :model do
-  it { should be_a(Twitterable) }
-end
+require 'rails_helper'
 
-# == Schema Information
-#
-# Table name: networks
-#
-#  id               :uuid             not null, primary key
-#  registered_by_id :uuid
-#  homepage         :string(255)
-#  name             :text
-#  slug             :string(255)
-#  twitter          :string(255)
-#  description      :text
-#  logo             :string(255)
-#  created_at       :datetime
-#  updated_at       :datetime
-#
+describe Network do
+  let(:network) do
+    Network.new
+  end
+end

9317248ee444115a9d1948ebd3ed7f7e220eed08
Mike Hall <mike@just3ws.com>
2014-12-05 22:53:21 -0600

    Added basic specs around csv/array handing in views

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#handle_array_or_csv' do
+    it 'receives a nil value' do
+      expect(helper.handle_array_or_csv(nil)).to be_nil
+    end
+
+    it 'receives an array' do
+      expect(helper.handle_array_or_csv(%w(hello goodbye))).to eq('hello, goodbye')
+    end
+
+    it 'receives a string' do
+      expect(helper.handle_array_or_csv('hello, goodbye')).to eq(%w(goodbye hello))
+    end
+
+    it 'receives a number' do
+      expect { helper.handle_array_or_csv(1) }.to raise_error("Can't handle what I can't recognize Fixnum is not an Array or String")
+    end
+  end
+end

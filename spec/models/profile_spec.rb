99035405acd2f2ff4fc1697e94d39e916697d6c4
Mike Hall <mike@just3ws.com>
2014-12-05 22:39:05 -0600

    Expanding tests around the user_group and profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,17 +1,41 @@
-require 'rails_helper'
-
 describe Profile do
   it { should be_a(Twitterable) }
 
-  let(:profile) do
-    Profile.new(first_name: 'hello', last_name: 'goodbye')
-  end
+  let (:user) { User.create(email: 'test@example.com', password: 'password', password_confirmation: 'password', username: 'testuser') }
+  let (:profile) { Profile.new(user: user, first_name: 'hello', last_name: 'goodbye') }
+
+  it { expect(profile).to be_valid }
 
   it '#full_name' do
     expect(profile.full_name).to eq('hello goodbye')
   end
 
-  it '#full_name_or_username' do
-    expect(profile.full_name_or_username).to eq('hello goodbye')
+  context 'geocode queries' do
+    it 'geocodes by city and country' do
+      profile = Profile.create(address: 'Chicago, US')
+      expect(profile.formatted_address).to eq('Chicago, IL, USA')
+      expect(profile.city).to eq('Chicago')
+      expect(profile.country).to eq('US')
+      expect(profile.address).to eq('Chicago, US')
+      expect(profile.latitude).to eq(41.8781136)
+      expect(profile.longitude).to eq(-87.6297982)
+    end
+  end
+
+  context 'full_name is present' do
+    it '#full_name_or_username' do
+      expect(profile.full_name_or_username).to eq('hello goodbye')
+    end
+  end
+
+  context 'full_name is blank' do
+    before {
+      profile.first_name = nil
+      profile.last_name = nil
+    }
+
+    it '#full_name_or_username' do
+      expect(profile.full_name_or_username).to eq('testuser')
+    end
   end
 end
--- spec/models/user_group_spec.rb
+++ spec/models/user_group_spec.rb
@@ -1,20 +1,54 @@
 require 'rails_helper'
 
 describe UserGroup do
-  let(:user_group) do
-    UserGroup.new(shortname: 'hello')
+  let(:user_group) {
+    UserGroup.new(
+      city: 'Chicago',
+      country: 'US',
+      name: 'Test User-Group',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '
+    )
+  }
+
+  it { expect(user_group).to be_valid }
+
+  context 'shortname is present' do
+    before {
+      user_group.shortname = 'hello'
+    }
+
+    it '#slug_candidates' do
+      expect(user_group.slug_candidates).to eq([
+        :shortname,
+        [:shortname, :city],
+        [:shortname, :city, :state_province],
+        [:shortname, :city, :state_province, :country]
+      ])
+    end
   end
 
-  it '#slug_candidates' do
-    expect(user_group.slug_candidates).to eq([
-      :shortname,
-      [:shortname, :city],
-      [:shortname, :city, :state_province],
-      [:shortname, :city, :state_province, :country]
-    ])
+  context 'shortname is blank' do
+    before {
+      user_group.shortname = nil
+    }
+
+    it '#slug_candidates' do
+      expect(user_group.slug_candidates).to eq([
+        :name,
+        [:name, :city],
+        [:name, :city, :state_province],
+        [:name, :city, :state_province, :country]
+      ])
+    end
   end
 
-  it '#address' do
-    expect(user_group.address).to eq(nil)
+  context 'no address is provided' do
+    it '#address' do
+      expect(user_group.address).to eq('Chicago, US')
+    end
+  end
+
+  context 'minimum valid address is set' do
+
   end
 end

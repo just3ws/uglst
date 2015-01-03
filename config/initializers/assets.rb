2ed7f325575dd8175e6a055065f85b220b0afc9d
Mike Hall <mike@just3ws.com>
2015-01-03 00:00:15 -0600

    Added CSS files for controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,17 +1,26 @@
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
 Rails.application.config.assets.precompile += %w(
+  account.css
   account.js
   happy.css
-  pages.css
   happy.js
+  happy/hello.css
   happy/hello.js
+  networks.css
   networks.js
+  pages.css
   pages.js
+  personal.css
   personal.js
+  profiles.css
   profiles.js
+  public.css
   public.js
+  registrations.css
   registrations.js
+  sessions.css
   sessions.js
+  user_groups.css
   user_groups.js
 )

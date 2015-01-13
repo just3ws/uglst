9bbe82665b0cd6cb6b9ac7b1a37e3c3363d186fd
Mike Hall <mike@just3ws.com>
2015-01-13 00:17:39 -0600

    Changed source for git

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'uglst'
-set :repo_url, 'git@bitbucket.org:ugtastic/ugtastic-public.git'
+set :repo_url, 'git@github.com:ugtastic/uglst.git'
 set :deploy_to, '/var/ugtastic/uglst'
 set :linked_files, %w(config/database.yml config/newrelic.yml config/secrets.yml .env)
 set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
@@ -10,27 +10,6 @@ set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.2.0@uglst'
 
 set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
-
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-end
 
 task :notify_rollbar do
   on roles(:app) do |_h|

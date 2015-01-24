a406479c41b0b2a8105e63d85c2ba27ba40ca257
Mike Hall <mike@just3ws.com>
2015-01-23 23:27:09 -0600

    Added devise controller helpers

---
+++ spec/support/controller_helpers.rb
@@ -0,0 +1,26 @@
+module ControllerHelpers
+  def login_with(user = double('user'), scope = :user)
+    current_user = "current_#{scope}".to_sym
+    if user.nil?
+      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, scope: scope)
+      allow(controller).to receive(current_user).and_return(nil)
+    else
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(current_user).and_return(user)
+    end
+  end
+end
+
+require 'devise'
+
+RSpec.configure do |c|
+  c.include Devise::TestHelpers, type: :controller
+  c.include Warden::Test::Helpers
+
+  c.include ControllerHelpers, type: :controller
+  Warden.test_mode!
+
+  c.after do
+    Warden.test_reset!
+  end
+end
